0ba2aa8ad17b79ea35e0adb92006fce9c419bc38
amirhossein-moloki <amirh.moloki@gmail.com>
2025-04-30 15:28:22 +0300

    modified the entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo "Running migrations..."
+echo "Running migrations...."
 python manage.py migrate
 
-echo "Starting server..."
+echo "Starting server...."
 python manage.py runserver 0.0.0.0:8000
